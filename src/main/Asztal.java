2b3473474e5de4b980f3fb756e1c6a9c574db4de
Villo05 <nemethvillo.2005@gmail.com>
2026-04-24 09:51:27 +0200

    kiiras, rajzol, inditas

--- src/main/Asztal.java
+++ src/main/Asztal.java
@@ -6,7 +6,44 @@ public class Asztal {
     private Sor[] sorok = new Sor[4];
     private int db = 0;
     
+    public void hozzaad(Sor sor){
+        if (db < 4){
+            sorok[db] = sor;
+            db++;
+        }else{
+            System.out.println("tele van az asztal");
+        }
+    }
     
+    public void kiir(){
+        System.out.println("\nSörök: ");
+        for (int i = 0; i < db; i++){
+            System.out.println(sorok[i]);
+        }
+    }
+    
+    public void rajzol(){
+        System.out.println("\nAsztal: ");
+        
+        System.out.print("|");
+        for (int i = 0; i < 2; i++){
+            if (i < db) {
+                System.out.print("o ");
+            }else {
+                System.out.print("  ");
+            }
+        }
+        System.out.println("|");
+        
+        System.out.print("|");
+        for (int i = 2; i < 4; i++){
+            if (i < db){
+                System.out.print("o ");
+            }else {
+                System.out.print("  ");
+            }
+        }
+        System.out.println("|");
+    }
 
-    
 }
--- src/main/Inditas.java
+++ src/main/Inditas.java
@@ -3,6 +3,23 @@ package main;
 
 public class Inditas {
     public static void main(String[] args) {
+      Asztal asztal = new Asztal();
       
+      Sor[] alapSorok = new Sor[4];
+        alapSorok[0] = new Sor(5.0, 500, "világos");
+        alapSorok[1] = new Sor(6.5, 400, "barna");
+        alapSorok[2] = new Sor(4.8, 330, "világos");
+        alapSorok[3] = new Sor(7.2, 200, "barna");
+
+        int db = (int)(Math.random() * 5);
+
+        for (int i = 0; i < db; i++) {
+            int index = (int)(Math.random() * 4);
+            asztal.hozzaad(alapSorok[index]);
+        }
+      
+      asztal.kiir();
+      asztal.rajzol();
+
     }
 }
